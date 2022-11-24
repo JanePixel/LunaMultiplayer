4f3f233f4d1435205d2707af6fa0f70b2c4851dc
JanePixel <janepixel42@gmail.com>
2022-11-24 11:00:29 +0000

    Changed broadcast interval to 6 minutes

--- Server/Custom/Models/BroadcasterWheel.cs
+++ Server/Custom/Models/BroadcasterWheel.cs
@@ -12,7 +12,7 @@ namespace Server.Custom.Models
         {
         }
 
-        public readonly int TimeBetweenBroadcastsInMilliseconds = 240000;
+        public readonly int TimeBetweenBroadcastsInMilliseconds = 360000;
 
         public int SelectedMessage = 0;
 
